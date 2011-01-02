1f179d2f259a916fff5e2695dff441504fab4ca5
Roger Braun <davinelulinvega@gmail.com>
2011-01-02 02:52:15 +0100

    show related entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,6 +6,9 @@ class Entry < ActiveRecord::Base
     self.defs_array.first + (self.defs_array.size > 1 ? "...": "")
   end
 
+  def short_html
+    pos_html(genus_html(short_definition))
+  end
 
   def pos
     self.definition.match(/POS: (.)/)[1]
@@ -17,10 +20,33 @@ class Entry < ActiveRecord::Base
     t.empty? || t == nil ? [p] : t
   end
 
+  def related
+    if self.entry_relation["HE"] then
+      res = Entry.where(:entry_relation => self.writing + "\n")
+    else
+      res = Entry.where(:writing => self.entry_relation.strip)
+    end
+    res || []
+  end
+
   private
+
+  CLEAN_REGEXPS = [/\{<.+?>\}/]
 
   def pos_and_def
     self.definition.match(/^(\(.+?\))(.*)/)[1..-1] 
+  end
+
+  def genus_html(defi)
+    defi.gsub(/<Gen.: (\w+)>/) do |mat|
+      " <em>#{$1}</em>"
+    end
+  end
+
+  def pos_html(defi)
+    defi.gsub(/\(<POS: (\w+).>\)/) do |mat|
+      " <strong>#{$1}</strong>"
+    end
   end
 
 end
