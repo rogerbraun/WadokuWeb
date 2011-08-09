34f79c797adcbcb6c1bc38058e3a4b12ecc2fab8
Roger Braun <roger.braun@student.uni-tuebingen.de>
2011-08-09 10:43:38 +0200

    Revert "do some caching for FAST FAST FAST searches"
    This reverts commit 6efae2c7f0dad1712e95cc4ab3cf61ff63fc1bf5.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,18 @@
 class SearchController < ApplicationController
-
-
   def index
     redirect_to "search#start" unless params[:search]
-    unless read_fragment({:page => params[:page] || 1, :search => params[:search]})
-      params[:search].to_kana!
-      @entries = Entry.search_by_any(params[:search], params[:page], 30) 
+    params[:search].to_kana!
+    @search = Entry.search_by_any(params[:search], params[:page], 30) 
+    @entries = @search
+    if @entries.count == 0 then
+      flash[:notice] = t("search.nothing_found")
+      redirect_to search_path
+    else
+      respond_to do |format|
+        format.html
+        format.js
+      end 
     end
-    
-    respond_to do |format|
-      format.html
-      format.js
-    end 
   end
 
   def start
