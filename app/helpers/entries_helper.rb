99fa002fff7ec0a38105ce1dad5714f2845cb6f3
Roger Braun <roger.braun@student.uni-tuebingen.de>
2011-08-15 14:01:56 +0200

    simple pagination with picky

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,6 +1,10 @@
 module EntriesHelper
 
   def paginate(page, total, step = 30)
-    "<b>#{total}</b>".html_safe
+    pages = (total / step.to_f).ceil
+    ( "<nav class='pagination'>" + 
+      1.upto(pages).map{|n| "<span class='page#{ page == n ? ' current' : ""}'>#{link_to(n.to_s, search_index_path(:page => n, :search => params[:search]))}</span>"}.join("") +
+      "</nav>"
+    ).html_safe
   end
 end
