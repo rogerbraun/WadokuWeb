d6fdbecdc0d7c2a9d74266b9a04b8f7a5d1b5d69
Roger Braun <roger@rogerbraun.net>
2011-08-26 16:14:24 +0200

    add endless scroll

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -3,7 +3,7 @@ module EntriesHelper
   def paginate(page, total, step = 30)
     pages = (total / step.to_f).ceil
     ( "<nav class='pagination'>" + 
-      1.upto(pages).map{|n| "<span class='page#{ page == n ? ' current' : ""}'>#{link_to(n.to_s, search_index_path(:page => n, :search => params[:search]))}</span>"}.join("") +
+      1.upto(pages).map{|n| "<span class='page#{ page == n ? ' current' : ""}'>#{link_to(n.to_s, search_index_path(:page => n, :search => params[:search]))}</span>"}.join("") + (page < pages ? "<span class='next'>#{link_to(">>", search_index_path(:page=> page + 1, :search => params[:search]), :class => "next")}</span>" : "") +
       "</nav>"
     ).html_safe
   end
