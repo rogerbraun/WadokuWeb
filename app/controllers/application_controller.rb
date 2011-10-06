12b3c6ce424d2beebbc35282f8e41e4b1d28a651
Roger Braun <davinelulinvega@gmail.com>
2011-10-06 15:19:42 +0200

    nice redesign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :prepare_for_mobile
+  #before_filter :prepare_for_mobile
   helper_method :mobile_device?
 
   private
