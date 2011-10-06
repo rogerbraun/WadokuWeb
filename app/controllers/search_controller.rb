dc366378b135e35516e3256ab886eca944c10c6a
Roger Braun <davinelulinvega@gmail.com>
2011-10-06 13:12:39 +0200

    first steps

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,42 +1,50 @@
 require "open-uri"
 class SearchController < ApplicationController
   def index
-    redirect_to "search#start" unless params[:search]
 
-    params[:page] ||= 1
-    @page = params[:page].to_i
-    #@search = Entry.search_by_any(params[:search], params[:page], 30) 
-    #@entries = @search
+    if params[:search] and params[:search] != "" then 
+      params[:page] ||= 1
+      @page = params[:page].to_i
+      #@search = Entry.search_by_any(params[:search], params[:page], 30) 
+      #@entries = @search
 
-    results = EntrySearch.search params[:search], :ids => 30, :offset => (@page - 1) * 30
+      results = EntrySearch.search params[:search], :ids => 30, :offset => (@page - 1) * 30
 
-    if results[:total] == 0 then
-      keizai = WadokuKeizai.search params[:search]
-      flash[:notice] = t("search.nothing_found").html_safe
-      unless keizai.empty? then
-        flash[:notice] = t("wadokukeizai.result", :count => keizai.size, :link => keizai.search_link).html_safe
-      end
-      redirect_to search_path
-    else
-      results.extend Picky::Convenience
-      @search_time = results[:duration]
+      if results[:total] == 0 then
+        keizai = WadokuKeizai.search params[:search]
+        flash[:notice] = t("search.nothing_found").html_safe
+        unless keizai.empty? then
+          flash[:notice] = t("wadokukeizai.result", :count => keizai.size, :link => keizai.search_link).html_safe
+        end
+        redirect_to search_path
+      else
+        results.extend Picky::Convenience
+        @search_time = results[:duration]
 
-      @total = results.total
-      @entries = results.ids.map{|id| Entry.find_by_wadoku_id(id)}.compact
-      respond_to do |format|
-        format.html
-        format.js
-        format.json { if params[:full] == "true" then 
-                        render :json  => {:total => results[:total], :from =>  ((@page - 1) * 30), :entries => @entries.map{|entry| {:daid => entry.wadoku_id, :midashigo => entry.writing, :kana => entry.kana, :german => entry.definition, :german_html => entry.full_html} }}
-                      else
-                        render :json => {:total => results[:total], :search_link => url_for(:action => :index, :search => params[:search])}
-                      end
-                    }
-
-        format.xml  { render :xml => @entries }
+        @total = results.total
+        @entries = results.ids.map{|id| Entry.find_by_wadoku_id(id)}.compact.uniq
+        @entries_left = @entries[0..@entries.size / 2]
+        @entries_right = @entries[(@entries.size / 2) + 1..-1]
       end 
+        respond_to do |format|
+          format.html
+          format.js
+          format.json { if params[:full] == "true" then 
+                          render :json  => {:total => results[:total], :from =>  ((@page - 1) * 30), :entries => @entries.map{|entry| {:daid => entry.wadoku_id, :midashigo => entry.writing, :kana => entry.kana, :german => entry.definition, :german_html => entry.full_html} }}
+                        else
+                          render :json => {:total => results[:total], :search_link => url_for(:action => :index, :search => params[:search])}
+                        end
+                      }
+
+          format.xml  { render :xml => @entries }
+        end
+    else
+      respond_to do |format|
+        format.html{ render :start}
+      end
     end
   end
+  
   
 
   def start
