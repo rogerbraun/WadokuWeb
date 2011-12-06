da2fd2e5caa1aefc09d9800c2463b6c38a92f3bd
Roger Braun <davinelulinvega@gmail.com>
2011-12-06 02:59:50 +0100

    add gravatars, editing in admin-mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def gravatar_link(size = 30, default = "identicon")
+    digest = Digest::MD5.hexdigest(self.email.downcase)
+    "http://www.gravatar.com/avatar/#{digest}?s=#{size}&d=#{default}"
+  end
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,23 +3,6 @@ require "spec_helper"
 
 describe Entry do
 
-#  before do 
-#    @aoidomoe = Entry.new
-#    @aoidomoe.definition = " <MGr: <TrE: <HW n: Malvenwappen>> (<Expl.: ein japan. Familienwappen mit kreisförmig angeordneten Malvenblättern>；<Expl.: von der Familie Tokugawa verwendet>；<Pict.: <Capt.: Malvenwappen><FileN: aoi>>)>."
-#
-#  end
-#
-#  it "should do some small modifications on the html" do
-#    
-#    @aoidomoe.to_html.should == "<span class='mg_nr'><span> <span class='tre'><span class='genus n'>Malvenwappen</span> </span> (<span class='expl'>ein japan. Familienwappen mit kreisförmig angeordneten Malvenblättern</span>; <span class='expl'>von der Familie Tokugawa verwendet</span>; ).<span class='svg_image'><span class='image_caption'>Malvenwappen</span><span class='svg'><a href='svg/aoi.svg'><img src='svg/aoi.svg' type='image/svg+xml' /></a></span></span>"
-#    
-#
-#  end
-#
-#  it "should parse" do
-#    @aoidomoe.parse.should_not include "Fehler"
-#  end
-
   it "should be versioned" do
 
     entry = Factory(:entry)
